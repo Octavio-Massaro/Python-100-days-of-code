940510bdd76b5ec71c6764be5a892d0123c0a4fa
Octavio-Massaro <octaviomassaro@gmail.com>
2023-03-13 14:43:38 -0300

    Enhance some mechanics in snake game

--- snake_game/main.py
+++ snake_game/main.py
@@ -26,19 +26,20 @@ while games_is_on:
     time.sleep(0.1)
     snake.move()
 
+    if snake.head.xcor() > 300 or snake.head.xcor() < -300 or snake.head.ycor() > 300 or snake.head.ycor() < -300:
+        score.reset_score()
+        snake.reset_snake()
+
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            score.reset_score()
+            snake.reset_snake()
+
     if snake.head.distance(food) < 15:
         food.refresh()
         snake.extend()
         score.increase_score()
 
-    if snake.head.xcor() > 300 or snake.head.xcor() < -300 or snake.head.ycor() > 300 or snake.head.ycor() < -300:
-        games_is_on = False
-        score.game_over()
-
-    for segment in snake.segments[1:]:
-        if snake.head.distance(segment) < 10:
-            games_is_on = False
-            score.game_over()
 
 
 screen.exitonclick()
--- snake_game/score_board.py
+++ snake_game/score_board.py
@@ -3,23 +3,40 @@ from turtle import Turtle
 ALIGNMENT = "center"
 FONT = ("Arial", 16, "normal")
 
+with open("data.txt") as file:
+    current_high_score = file.read()
+    current_high_score = int(current_high_score)
+
 
 class ScoreBoard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        self.high_score = current_high_score
         self.color("white")
         self.hideturtle()
         self.penup()
         self.goto(0, 270)
-        self.increase_score()
+        self.show_score()
 
     def increase_score(self):
-        self.clear()
-        message = f"Score: {self.score}"
-        self.write(font=FONT, align=ALIGNMENT, move=False, arg=message)
         self.score += 1
+        self.show_score()
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write(font=FONT, align=ALIGNMENT, move=False, arg="GAME OVER")
+    def show_score(self):
+        self.clear()
+        message = f"Score: {self.score} High Score: {self.high_score}"
+        self.write(font=FONT, align=ALIGNMENT, move=False, arg=message)
+
+    def reset_score(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open("data.txt", "w") as file2:
+                file2.write(str(self.high_score))
+        self.score = 0
+        self.show_score()
+
+
+    # def game_over(self):
+    #     self.goto(0, 0)
+    #     self.write(font=FONT, align=ALIGNMENT, move=False, arg="GAME OVER")
